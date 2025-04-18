8cca1b66577595fa102e82b891c7021e8452e636
Chumki-Adak <164296874+Chumki-Adak@users.noreply.github.com>
2025-04-18 16:18:18 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-const dbUri = 'mongodb+srv://chumki:1234@cluster0.rlhfooc.mongodb.net/user_db?retryWrites=true&w=majority&appName=Cluster0'
+const dbUri = 'mongodb+srv://@cluster0.rlhfooc.mongodb.net/user_db?retryWrites=true&w=majority&appName=Cluster0'
 
 module.exports = () => mongoose.connect(dbUri)
